508b6b808d05ef7dda6f76177bdb9f131d851d0d
Edgardo Luis Jimenez <edgardotecno@gmail.com>
2023-03-23 20:16:34 -0500

    merge mobile menu and shopping cart

--- main.js
+++ main.js
@@ -1,8 +1,49 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const burguerMenu = document.querySelector('.menu')
+const menuCarrito = document.querySelector('.navbar-shopping-cart')
+const mobileMenu = document.querySelector('.menu-mobile')
+const aside = document.querySelector('.product-detail')
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
+burguerMenu.addEventListener('click', toggleMobileMenu)
+menuCarrito.addEventListener('click', toggleCarritoAside)
 
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive')
+
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive')
+    }
+
+
     desktopMenu.classList.toggle('inactive')
+}
+
+function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive')
+
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive')
+    }
+
+    mobileMenu.classList.toggle('inactive')
+}
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
+    
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive')
+    }
+
+    if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add('inactive')
+    }
+    
+    aside.classList.toggle('inactive')
 }
